1094a6e1ab38b58d1d10420eb9703612a1cafe93
negarjf <n.jamalifard@gmail.com>
2017-06-02 01:14:54 +0430

    update
    completed

--- js/custom.js
+++ js/custom.js
@@ -5,4 +5,11 @@ jQuery(function($){
 		$('#navbar').slideToggle();
 	});
 
+	$('.tile-hover').on('mouseenter',function(){
+		$(this).find('.caption').fadeIn().css('display','inline-block');
+	});
+	$('.tile-hover').on('mouseleave',function(){
+		$(this).find('.caption').fadeOut().css('display','inline-block');
+	});
+
 });
