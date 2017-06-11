3bf049e2a5c5219d50ca1b352c7a97b673a8cf6b
negarjf <n.jamalifard@gmail.com>
2017-06-11 20:43:31 +0430

    links behavior updated

--- js/custom.js
+++ js/custom.js
@@ -12,4 +12,32 @@ jQuery(function($){
 		$(this).find('.caption').fadeOut().css('display','inline-block');
 	});
 
+	$(window).on('scroll', function(){
+
+		if($(document).scrollTop() > 200){
+			$('nav').addClass('nav-fixed');
+		}else{
+			$('nav').removeClass('nav-fixed');
+		}
+	});
+
+	$('#navbar a').on('click',function(event){
+
+		if(this.hash !=""){
+			event.preventDefault();
+
+			var hash = this.hash;
+
+			$("html, body").animate({
+
+				scrollTop: $(hash).offset().top - 120
+
+			},800, function(){
+				window.location.hash = hash;
+			});
+		} //end if
+
+	});
+
+
 });
